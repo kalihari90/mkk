784009df871fbf58d2ee437ccc83217a9406cfcc
kaLihaRi <kalihari90@gmail.com>
2023-11-09 20:31:29 +0100

    modifying hero and services sections, adding two about us sections

--- js/main.js
+++ js/main.js
@@ -1,16 +1,41 @@
-const mobileNav = document.querySelector(".nav")
-const burgerBtn = document.querySelector(".hamburger")
-const buttonOne = document.querySelector(".button-one")
+const mobileNav = document.querySelector(".nav");
+const burgerBtn = document.querySelector(".hamburger__button");
+const navLogo = document.querySelector(".logo__img");
 
-burgerBtn.addEventListener("click", function () {
-    mobileNav.classList.toggle("active")
-})
+// BURGER ICON - switch mobile navigation on/off
+burgerBtn.addEventListener("click", () => {
+	mobileNav.classList.toggle("active");
+});
 
-buttonOne.addEventListener("click", () => {
-    const isOpened = buttonOne.getAttribute("aria-expanded");
-    if (isOpened === "false") {
-        buttonOne.setAttribute("aria-expanded", "true")
-    } else {
-        buttonOne.setAttribute("aria-expanded", "false")
-    }
-})
+burgerBtn.addEventListener("click", () => {
+	const isOpened = burgerBtn.getAttribute("aria-expanded");
+	if (isOpened === "false") {
+		burgerBtn.setAttribute("aria-expanded", "true");
+	} else {
+		burgerBtn.setAttribute("aria-expanded", "false");
+	}
+});
+
+// MOBILE NAV - close navigation after click anywhere on it
+mobileNav.addEventListener("click", () => {
+	mobileNav.classList.remove("active");
+});
+
+mobileNav.addEventListener("click", () => {
+	const isOpened = burgerBtn.getAttribute("aria-expanded");
+	if (isOpened === "true") {
+		burgerBtn.setAttribute("aria-expanded", "false");
+	}
+});
+
+// LOGO - close navigation after click
+navLogo.addEventListener("click", () => {
+	mobileNav.classList.remove("active");
+});
+
+navLogo.addEventListener("click", () => {
+	const isOpened = burgerBtn.getAttribute("aria-expanded");
+	if (isOpened === "true") {
+		burgerBtn.setAttribute("aria-expanded", "false");
+	}
+});
