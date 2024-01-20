ea11bb0ebf3105627aa8618eac451a3c1f1dc2a6
kaLihaRi <kalihari90@gmail.com>
2024-01-20 23:51:33 +0100

    massive update, mostly new graphics

--- js/main.js
+++ js/main.js
@@ -39,3 +39,52 @@ navLogo.addEventListener("click", () => {
 		burgerBtn.setAttribute("aria-expanded", "false");
 	}
 });
+
+// COUNTER - counting numbers animation
+
+document.addEventListener("DOMContentLoaded", function () {
+	const counters = document.querySelectorAll(".counter");
+
+	const isElementInViewport = el => {
+		const rect = el.getBoundingClientRect();
+		return (
+			rect.top >= 0 &&
+			rect.left >= 0 &&
+			rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+			rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+		);
+	};
+
+	const handleScroll = () => {
+		counters.forEach(counter => {
+			if (isElementInViewport(counter)) {
+				const target = +counter.getAttribute("data-target");
+				const duration = +counter.getAttribute("data-duration") || 2;
+
+				if (!counter.hasAttribute("data-animated")) {
+					const updateCount = () => {
+						const count = +counter.innerText;
+						const speed = target / duration;
+
+						if (count < target) {
+							counter.innerText = Math.ceil(count + speed);
+							setTimeout(updateCount, 1000 / 60);
+						} else {
+							counter.innerText = target;
+							counter.removeAttribute("data-animated");
+						}
+					};
+
+					updateCount();
+					counter.setAttribute("data-animated", "true");
+				}
+			}
+		});
+	};
+
+	// Uruchom funkcję handleScroll po przewinięciu strony
+	window.addEventListener("scroll", handleScroll);
+
+	// Uruchom funkcję handleScroll od razu, aby obsłużyć przypadki, gdy elementy są już w widoku przy załadowaniu strony
+	handleScroll();
+});
